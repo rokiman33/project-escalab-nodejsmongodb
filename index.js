3f06d1ac38cd6f868f8dfd645541ac06a00ddcf7
Richard Diaz <34225851+rokiman33@users.noreply.github.com>
2021-10-02 00:41:04 -0300

    Update index.js

--- index.js
+++ index.js
@@ -13,6 +13,6 @@ app.use(express.json());
 
 app.use('/api/productos', require('./routes/producto'));
 
-app.listen(80,() => {
+app.listen(3000,() => {
     console.log('El servidor esta corriendo perfectamente')
 })
