6b19431908e08ebd9b62c9ce79969b11cb7e2769
Richard Diaz <34225851+rokiman33@users.noreply.github.com>
2021-10-02 00:48:31 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const conectarDB = require('./config/db');
 const cors = require("cors");
+require("dotenv").config();
 
 // Creamos el servidor
 const app = express();
@@ -13,6 +14,9 @@ app.use(express.json());
 
 app.use('/api/productos', require('./routes/producto'));
 
-app.listen(3000,() => {
-    console.log('El servidor esta corriendo perfectamente')
+// port
+const port = process.env.PORT || 8400;
+
+app.listen(port,() => {
+    console.log('El servidor esta corriendo perfectamente ${port}')
 })
